791a254c46319f0a4a6f109769724465c610059e
Eduardo <edfadeon@gmail.com>
2021-03-09 10:41:49 -0300

    criando api simulacao

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,16 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb(){
+    const categorias = [
+
+      { id: 1, nome: "Moradia", descricao: "Pagamentos de contas da casa" },
+      { id: 2, nome: "Saúde", descricao: "Planos de saúde" },
+      { id: 3, nome: "Lazer", descricao: "cinema, parques, praia, etc" },
+      { id: 4, nome: "Salário", descricao: "Recebimento do salário" },
+      { id: 5, nome: "Autonomo", descricao: "Trabalhos por conta própria" }
+
+    ];
+    return {categorias}
+  }
+}
