af83d7878cb886b547850fb3fa77a55376f6ddee
Eduardo <edfadeon@gmail.com>
2021-03-10 12:43:04 -0300

    configurando modulo entries

---
+++ src/app/paginas/entries/shared-entries/entry.model.ts
@@ -0,0 +1,26 @@
+
+import { Categoria } from '../../categorias/shared/categoria.model';
+export class Entry {
+
+  constructor(
+    public id?: number,
+    public nome?: string,
+    public descricao?: string,
+    public tipo?: string,
+    public quantidade?: string,
+    public data?: string,
+    public pago?: boolean,
+    public categoriaId?: number,
+    public categoria?: Categoria
+  ){}
+
+  static types = {
+    expense: 'Despesa',
+    revenue: 'Receita'
+  };
+
+  getTextoPago(): string {
+    return this.pago ? 'pago' : 'pendente';
+
+  }
+}
